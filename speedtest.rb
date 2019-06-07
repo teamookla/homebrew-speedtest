dd36b080140de7f8622937fe40c7ba41b39b8fe6
Kevin Nuckolls <kevin.nuckolls@gmail.com>
2019-06-07 11:21:20 -0500

    0.9.2 sha256 fix

--- speedtest.rb
+++ speedtest.rb
@@ -2,7 +2,7 @@ class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://www.speedtest.net/"
   url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-0.9.2.2-1.e6f01d3-macosx.tgz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "2756d2b065b376e2d65786b1d9b695ccdfb55dca6590100cb59e8d14cf9104b0"
 
   def install
     bin.install "./speedtest"
