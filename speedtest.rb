028100e38c9075688fea4599a4e963d04d6b64e9
Linux User <jenkins@ip-172-16-23-23.us-west-2.compute.internal>
2019-07-08 16:33:50 +0000

    version bump to 0.9.3.92

--- speedtest.rb
+++ speedtest.rb
@@ -1,8 +1,8 @@
 class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://www.speedtest.net/"
-  url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-0.9.2.2-1.e6f01d3-macosx.tgz"
-  sha256 "2756d2b065b376e2d65786b1d9b695ccdfb55dca6590100cb59e8d14cf9104b0"
+  url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-$CURRENT_VERSION_NUMBER-$CURRENT_BUILD_NUMBER-macosx.tgz"
+  sha256 "$SPEEDTEST_SHA256"
 
   def install
     bin.install "./speedtest"
