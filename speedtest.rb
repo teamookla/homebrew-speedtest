2dcf1f702c45adb7fb358f2c6b89b72e3550c624
Kevin Nuckolls <kevin.nuckolls@gmail.com>
2019-07-12 18:08:18 -0500

    correct sha for 0.9.3

--- speedtest.rb
+++ speedtest.rb
@@ -2,7 +2,7 @@ class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://www.speedtest.net/"
   url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-0.9.3.110-1.8fbb8e4-macosx.tgz"
-  sha256 "463e5043b92de8d7fd4b497c1ee414fb51a1849b12500e9003f6d9eae556bf67"
+  sha256 "d38cfb44328b2f95df01cbf38c9f12bc434ae368f19ee266a2139a75b7d55f88"
 
   def install
     bin.install "./speedtest"
