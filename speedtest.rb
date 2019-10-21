9eef471d87cbbf211a3ea353978ebc656cb44b4f
Kevin Nuckolls <kevin.nuckolls@gmail.com>
2019-10-21 10:53:26 -0500

    version bump to 0.10.0.8

--- speedtest.rb
+++ speedtest.rb
@@ -1,8 +1,8 @@
 class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://www.speedtest.net/"
-  url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-$CURRENT_VERSION_NUMBER-$CURRENT_BUILD_NUMBER-macosx.tgz"
-  sha256 "$SPEEDTEST_SHA256"
+  url "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-0.10.0.8-1.eba90b3-macosx.tgz"
+  sha256 "f431f706f4bca113e366f902527dd7da2c6c887a04704b11fa17b3993e2e16ea"
 
   def install
     bin.install "./speedtest"
