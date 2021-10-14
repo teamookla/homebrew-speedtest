fe9cc0b39b1f81544196e4389d88421931c63fd0
Clayton Wong <claytonjwong@gmail.com>
2021-10-14 09:51:20 -0700

    update to CLI 1.1.0

--- speedtest.rb
+++ speedtest.rb
@@ -1,8 +1,8 @@
 class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://speedtest.net/apps/cli"
-  url "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-macosx.tgz"
-  sha256 "8d0af8a81e668fbf04b7676f173016976131877e9fbdcd0a396d4e6b70a5e8f4"
+  url "https://install.speedtest.net/app/cli/ookla-speedtest-1.1.0-macosx.tgz"
+  sha256 "481e38001c2ebe8d62d850c76d16676b5e246730f8e8dc1f1e0aa9f002ab6b9a"
 
   def install
     bin.install "./speedtest"
@@ -11,7 +11,7 @@ class Speedtest < Formula
   end
 
   test do
-    assert_match /1\.0\.0/, shell_output("#{bin}/speedtest --version")
+    assert_match /1\.1\.0/, shell_output("#{bin}/speedtest --version")
   end
 end
 
