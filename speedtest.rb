cb26cd9952f1b3361f1f984d71219445fa81ec52
Matt Tucker <tuck@whistlingfish.net>
2022-01-19 10:39:28 -0800

    release 1.1.1.84 (better macos compatibility, universal binary)

--- speedtest.rb
+++ speedtest.rb
@@ -1,9 +1,9 @@
 class Speedtest < Formula
   desc "Ookla Speedtest"
   homepage "https://speedtest.net/apps/cli"
-  url "https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-macosx-x86_64.tgz"
-  sha256 "b6d78d664f1a67a2ed214932b0f465b18c1529ae6e162e09acc99c12d9e1eafc"
-  version "1.1.1"
+  url "https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1.84-macosx-x86_64.tgz"
+  sha256 "153f76e4e502a2bb470c7f7c3b4b8afaa50eb98bb11e86216fcb1a8b92676e94"
+  version "1.1.1.84"
 
   def install
     bin.install "./speedtest"
@@ -12,7 +12,7 @@ class Speedtest < Formula
   end
 
   test do
-    assert_match /1\.1\.1/, shell_output("#{bin}/speedtest --version")
+    assert_match /\b1\.1\.1\.84\b/, shell_output("#{bin}/speedtest --version")
   end
 end
 
